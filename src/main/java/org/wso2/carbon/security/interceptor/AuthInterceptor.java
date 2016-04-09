5aab4118764e1ee49d9c14492ce334cf90c8edcf
thanujalk <jatljayasinghe@gmail.com>
2016-04-09 09:02:21 +0530

    Adding Auth Interceptor

---
+++ src/main/java/org/wso2/carbon/security/interceptor/AuthInterceptor.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.carbon.security.interceptor;
+
+import com.google.common.collect.ArrayListMultimap;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wso2.carbon.security.interceptor.annotation.Secure;
+import org.wso2.carbon.security.interceptor.util.Constants;
+import org.wso2.carbon.security.jaas.CarbonCallbackHandler;
+import org.wso2.carbon.security.jaas.CarbonPermission;
+import org.wso2.msf4j.HttpResponder;
+import org.wso2.msf4j.Interceptor;
+import org.wso2.msf4j.ServiceMethodInfo;
+
+import javax.security.auth.Subject;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.HttpMethod;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import java.lang.reflect.Method;
+import java.security.AccessControlException;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
+
+/**
+ * Authentication and Authorization interceptor for Carbon Admin Services
+ */
+@Component(
+        name = "org.wso2.carbon.security.interceptor.AuthInterceptor",
+        service = Interceptor.class,
+        immediate = true
+)
+public class AuthInterceptor implements Interceptor {
+
+    private static final Logger log = LoggerFactory.getLogger(AuthInterceptor.class);
+
+    @Override
+    public boolean preCall(HttpRequest httpRequest, HttpResponder httpResponder, ServiceMethodInfo serviceMethodInfo) {
+
+        CallbackHandler callbackHandler = new CarbonCallbackHandler(httpRequest);
+
+        LoginContext loginContext;
+        try {
+            loginContext = new LoginContext(Constants.DEFAULT_JAAS_CONFIG, callbackHandler);
+        } catch (LoginException e) {
+            log.error("Error occurred while initiating login context.", e);
+            sendInternalServerError(httpResponder);
+            return false;
+        }
+
+        try {
+            loginContext.login();
+        } catch (LoginException e) {
+            if (log.isDebugEnabled()) {
+                log.debug("Login Failed", e);
+            }
+            sendUnauthorized(httpResponder);
+            return false;
+        }
+
+        //TODO: Authorization also handled in this interceptor since we can't decide order of execution for interceptors
+
+        if (serviceMethodInfo.getMethod().isAnnotationPresent(Secure.class)) {
+
+            if (!this.isAuthorized(loginContext.getSubject(), buildCarbonPermission(serviceMethodInfo))) {
+                sendUnauthorized(httpResponder);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public void postCall(HttpRequest httpRequest, HttpResponseStatus httpResponseStatus,
+                         ServiceMethodInfo serviceMethodInfo) {
+
+    }
+
+    private boolean isAuthorized(Subject subject, final CarbonPermission requiredPermission) {
+
+        final SecurityManager securityManager;
+
+        if (System.getSecurityManager() == null) {
+            securityManager = new SecurityManager();
+        } else {
+            securityManager = System.getSecurityManager();
+        }
+
+        try {
+            Subject.doAsPrivileged(subject, (PrivilegedExceptionAction) () -> {
+                securityManager.checkPermission(requiredPermission);
+                return null;
+            }, null);
+            return true;
+        } catch (AccessControlException | PrivilegedActionException e) {
+            if (log.isDebugEnabled()) {
+                log.debug("Authorization Failed", e);
+            }
+            return false;
+        }
+    }
+
+    private void sendUnauthorized(HttpResponder httpResponder) {
+        httpResponder.sendStatus(HttpResponseStatus.UNAUTHORIZED, ArrayListMultimap.create());
+    }
+
+    private void sendInternalServerError(HttpResponder httpResponder) {
+        httpResponder.sendStatus(HttpResponseStatus.INTERNAL_SERVER_ERROR, ArrayListMultimap.create());
+    }
+
+    private CarbonPermission buildCarbonPermission(ServiceMethodInfo serviceMethodInfo) {
+
+        StringBuilder permissionBuilder = new StringBuilder();
+        permissionBuilder.append(serviceMethodInfo.getMethodName()).append(".")
+                .append(serviceMethodInfo.getMethod().getName());
+
+        return new CarbonPermission(permissionBuilder.toString(), getAction(serviceMethodInfo.getMethod()));
+    }
+
+    private String getAction(Method method) {
+
+        if (method.isAnnotationPresent(GET.class)) {
+            return HttpMethod.GET;
+        } else if (method.isAnnotationPresent(POST.class)) {
+            return HttpMethod.POST;
+        } else if (method.isAnnotationPresent(PUT.class)) {
+            return HttpMethod.PUT;
+        } else if (method.isAnnotationPresent(DELETE.class)) {
+            return HttpMethod.DELETE;
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/org/wso2/carbon/security/interceptor/util/Constants.java
@@ -0,0 +1,14 @@
+package org.wso2.carbon.security.interceptor.util;
+
+/**
+ * Constants
+ */
+public class Constants {
+
+    private Constants() {
+
+    }
+
+    public static final String DEFAULT_JAAS_CONFIG = "CarbonSecurityConfig";
+
+}
