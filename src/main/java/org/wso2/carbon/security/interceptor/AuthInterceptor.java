ff663b63dc91804634ef2df30b3c4c95aaa46f56
thanujalk <jatljayasinghe@gmail.com>
2016-05-18 12:20:37 +0530

    Adding Carbon-Message realted changes

--- src/main/java/org/wso2/carbon/security/interceptor/AuthInterceptor.java
+++ src/main/java/org/wso2/carbon/security/interceptor/AuthInterceptor.java
@@ -16,33 +16,21 @@
 
 package org.wso2.carbon.security.interceptor;
 
-import com.google.common.collect.ArrayListMultimap;
-import io.netty.handler.codec.http.HttpRequest;
-import io.netty.handler.codec.http.HttpResponseStatus;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.wso2.carbon.security.interceptor.annotation.Secure;
+import org.wso2.carbon.messaging.CarbonMessage;
+import org.wso2.carbon.security.caas.jaas.ProxyCallbackHandler;
+import org.wso2.carbon.security.interceptor.util.AuthCarbonMessage;
 import org.wso2.carbon.security.interceptor.util.Constants;
-import org.wso2.carbon.security.jaas.CarbonCallbackHandler;
-import org.wso2.carbon.security.jaas.CarbonPermission;
-import org.wso2.msf4j.HttpResponder;
 import org.wso2.msf4j.Interceptor;
+import org.wso2.msf4j.Request;
+import org.wso2.msf4j.Response;
 import org.wso2.msf4j.ServiceMethodInfo;
 
-import javax.security.auth.Subject;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.login.LoginContext;
 import javax.security.auth.login.LoginException;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.HttpMethod;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import java.lang.reflect.Method;
-import java.security.AccessControlException;
-import java.security.PrivilegedActionException;
-import java.security.PrivilegedExceptionAction;
 
 /**
  * Authentication and Authorization interceptor for Carbon Admin Services
@@ -57,16 +45,16 @@ public class AuthInterceptor implements Interceptor {
     private static final Logger log = LoggerFactory.getLogger(AuthInterceptor.class);
 
     @Override
-    public boolean preCall(HttpRequest httpRequest, HttpResponder httpResponder, ServiceMethodInfo serviceMethodInfo) {
+    public boolean preCall(Request request, Response response, ServiceMethodInfo serviceMethodInfo) throws Exception {
 
-        CallbackHandler callbackHandler = new CarbonCallbackHandler(httpRequest);
+        CallbackHandler callbackHandler = new ProxyCallbackHandler(getCarbonMessage(request));
 
         LoginContext loginContext;
         try {
             loginContext = new LoginContext(Constants.DEFAULT_JAAS_CONFIG, callbackHandler);
         } catch (LoginException e) {
             log.error("Error occurred while initiating login context.", e);
-            sendInternalServerError(httpResponder);
+            sendInternalServerError(response);
             return false;
         }
 
@@ -76,82 +64,98 @@ public class AuthInterceptor implements Interceptor {
             if (log.isDebugEnabled()) {
                 log.debug("Login Failed", e);
             }
-            sendUnauthorized(httpResponder);
+            sendUnauthorized(response);
             return false;
         }
 
-        //TODO: Authorization also handled in this interceptor since we can't decide order of execution for interceptors
-
-        if (serviceMethodInfo.getMethod().isAnnotationPresent(Secure.class)) {
-
-            if (!this.isAuthorized(loginContext.getSubject(), buildCarbonPermission(serviceMethodInfo))) {
-                sendUnauthorized(httpResponder);
-                return false;
-            }
-        }
-
-        return true;
+        return  true;
     }
 
     @Override
-    public void postCall(HttpRequest httpRequest, HttpResponseStatus httpResponseStatus,
-                         ServiceMethodInfo serviceMethodInfo) {
+    public void postCall(Request request, int i, ServiceMethodInfo serviceMethodInfo) throws Exception {
 
     }
 
-    private boolean isAuthorized(Subject subject, final CarbonPermission requiredPermission) {
-
-        final SecurityManager securityManager;
-
-        if (System.getSecurityManager() == null) {
-            securityManager = new SecurityManager();
-        } else {
-            securityManager = System.getSecurityManager();
-        }
-
-        try {
-            Subject.doAsPrivileged(subject, (PrivilegedExceptionAction) () -> {
-                securityManager.checkPermission(requiredPermission);
-                return null;
-            }, null);
-            return true;
-        } catch (AccessControlException | PrivilegedActionException e) {
-            if (log.isDebugEnabled()) {
-                log.debug("Authorization Failed", e);
-            }
-            return false;
-        }
+    private void sendUnauthorized(Response response) {
+        response.setStatus(401);
     }
 
-    private void sendUnauthorized(HttpResponder httpResponder) {
-        httpResponder.sendStatus(HttpResponseStatus.UNAUTHORIZED, ArrayListMultimap.create());
+    private void sendInternalServerError(Response response) {
+        response.setStatus(500);
     }
 
-    private void sendInternalServerError(HttpResponder httpResponder) {
-        httpResponder.sendStatus(HttpResponseStatus.INTERNAL_SERVER_ERROR, ArrayListMultimap.create());
+    private CarbonMessage getCarbonMessage(Request request) {
+        AuthCarbonMessage carbonMessage = new AuthCarbonMessage();
+        carbonMessage.setHeaders(request.getHeaders());
+        carbonMessage.setMessageBody(request.getMessageBody());
+        return carbonMessage;
     }
 
-    private CarbonPermission buildCarbonPermission(ServiceMethodInfo serviceMethodInfo) {
 
-        StringBuilder permissionBuilder = new StringBuilder();
-        permissionBuilder.append(serviceMethodInfo.getMethodName()).append(".")
-                .append(serviceMethodInfo.getMethod().getName());
+//
+//        //TODO: Authorization also handled in this interceptor since we can't decide order of execution for
+// interceptors
+//
+//        if (serviceMethodInfo.getMethod().isAnnotationPresent(Secure.class)) {
+//
+//            if (!this.isAuthorized(loginContext.getSubject(), buildCarbonPermission(serviceMethodInfo))) {
+//                sendUnauthorized(httpResponder);
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//    }
+//
 
-        return new CarbonPermission(permissionBuilder.toString(), getAction(serviceMethodInfo.getMethod()));
-    }
+//    private boolean isAuthorized(Subject subject, final CarbonPermission requiredPermission) {
+//
+//        final SecurityManager securityManager;
+//
+//        if (System.getSecurityManager() == null) {
+//            securityManager = new SecurityManager();
+//        } else {
+//            securityManager = System.getSecurityManager();
+//        }
+//
+//        try {
+//            Subject.doAsPrivileged(subject, (PrivilegedExceptionAction) () -> {
+//                securityManager.checkPermission(requiredPermission);
+//                return null;
+//            }, null);
+//            return true;
+//        } catch (AccessControlException | PrivilegedActionException e) {
+//            if (log.isDebugEnabled()) {
+//                log.debug("Authorization Failed", e);
+//            }
+//            return false;
+//        }
+//    }
+//
 
-    private String getAction(Method method) {
+//
+//    private CarbonPermission buildCarbonPermission(ServiceMethodInfo serviceMethodInfo) {
+//
+//        StringBuilder permissionBuilder = new StringBuilder();
+//        permissionBuilder.append(serviceMethodInfo.getMethodName()).append(".")
+//                .append(serviceMethodInfo.getMethod().getName());
+//
+//        return new CarbonPermission(permissionBuilder.toString(), getAction(serviceMethodInfo.getMethod()));
+//    }
+//
+//    private String getAction(Method method) {
+//
+//        if (method.isAnnotationPresent(GET.class)) {
+//            return HttpMethod.GET;
+//        } else if (method.isAnnotationPresent(POST.class)) {
+//            return HttpMethod.POST;
+//        } else if (method.isAnnotationPresent(PUT.class)) {
+//            return HttpMethod.PUT;
+//        } else if (method.isAnnotationPresent(DELETE.class)) {
+//            return HttpMethod.DELETE;
+//        }
+//        return null;
+//    }
 
-        if (method.isAnnotationPresent(GET.class)) {
-            return HttpMethod.GET;
-        } else if (method.isAnnotationPresent(POST.class)) {
-            return HttpMethod.POST;
-        } else if (method.isAnnotationPresent(PUT.class)) {
-            return HttpMethod.PUT;
-        } else if (method.isAnnotationPresent(DELETE.class)) {
-            return HttpMethod.DELETE;
-        }
-        return null;
-    }
 
 }
